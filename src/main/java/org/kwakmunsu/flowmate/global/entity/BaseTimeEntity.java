6fb1116e2c516c20a2a516e2b6791241f8bcdec8
kwakseobang <iii6602@naver.com>
2025-07-14 12:51:49 +0900

    [#1] Setting: BaseTimeEntity 생성

---
+++ src/main/java/org/kwakmunsu/flowmate/global/entity/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package org.kwakmunsu.flowmate.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", updatable = true)
+    private LocalDateTime updatedAt;
+
+}
