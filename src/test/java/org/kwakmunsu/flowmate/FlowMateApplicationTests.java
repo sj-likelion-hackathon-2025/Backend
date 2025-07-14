5ddc2f4e12d32cef337c4a2217657953e2e662fc
kwakseobang <iii6602@naver.com>
2025-07-14 11:04:54 +0900

    git initialize

---
+++ src/test/java/org/kwakmunsu/flowmate/FlowMateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kwakmunsu.flowmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
