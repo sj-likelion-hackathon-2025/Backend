13dd13d52be127a73a8cad50d39c9f66d463cb8c
kwakseobang <iii6602@naver.com>
2025-07-14 21:12:28 +0900

    [#3] Setting: MemberCategory Entity 및 열거형 생성

---
+++ src/main/java/org/kwakmunsu/flowmate/domain/member/entity/InterestCategory.java
@@ -0,0 +1,12 @@
+package org.kwakmunsu.flowmate.domain.member.entity;
+
+public enum InterestCategory {
+
+    EXERCISE,  // 운동
+    STUDY,     // 공부
+    DIET,      // 식단
+    LIFESTYLE, // 생활습관
+    FINANCE,   // 금융
+    PROJECT    // 프로젝트
+
+}
---
+++ src/main/java/org/kwakmunsu/flowmate/domain/member/entity/MemberCategory.java
@@ -0,0 +1,36 @@
+package org.kwakmunsu.flowmate.domain.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class MemberCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "member_id", nullable = false)
+    private Long memberId;
+
+    @Enumerated(value = EnumType.STRING)
+    private InterestCategory category;
+
+    @Builder
+    private MemberCategory(Long memberId, InterestCategory category) {
+        this.memberId = memberId;
+        this.category = category;
+    }
+
+}
