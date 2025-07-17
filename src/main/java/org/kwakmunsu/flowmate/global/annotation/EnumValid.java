fc9932b558694bbf5b37dd377f88398b66d03dd9
kwakseobang <iii6602@naver.com>
2025-07-17 23:09:06 +0900

    Feat: enum 유효성 검증 커스텀 어노테이션 생성

---
+++ src/main/java/org/kwakmunsu/flowmate/global/annotation/EnumValid.java
@@ -0,0 +1,24 @@
+package org.kwakmunsu.flowmate.global.annotation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = {EnumValidator.class})
+public @interface EnumValid {
+
+    String message() default "";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+
+    Class<? extends Enum<?>> enumClass();
+
+    boolean ignoreCase() default false;
+}
