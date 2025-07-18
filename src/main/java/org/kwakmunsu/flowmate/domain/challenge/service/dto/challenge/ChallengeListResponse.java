1ed4be789f919fdc980914570d81e9c7a3064e40
kwakseobang <iii6602@naver.com>
2025-07-18 13:09:06 +0900

    Refactor: Builder 추가

--- src/main/java/org/kwakmunsu/flowmate/domain/challenge/service/dto/challenge/ChallengeListResponse.java
+++ src/main/java/org/kwakmunsu/flowmate/domain/challenge/service/dto/challenge/ChallengeListResponse.java
@@ -2,8 +2,10 @@ package org.kwakmunsu.flowmate.domain.challenge.service.dto.challenge;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
+import lombok.Builder;
 
 @Schema(description = "챌린지 목록 조회 응답 DTO")
+@Builder
 public record ChallengeListResponse(
         @Schema(description = "챌린지 목록 조회 시 보여지는 정보")
         List<ChallengePreviewResponse> challengePreviewResponses,
