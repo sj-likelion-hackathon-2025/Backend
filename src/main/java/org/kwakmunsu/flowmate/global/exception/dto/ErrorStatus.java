387c86a6f1426d924401e3f5873937ca33583143
kwakseobang <iii6602@naver.com>
2025-07-19 18:01:05 +0900

    Setting: 챌린지 신청 예외 메세지 추가

--- src/main/java/org/kwakmunsu/flowmate/global/exception/dto/ErrorStatus.java
+++ src/main/java/org/kwakmunsu/flowmate/global/exception/dto/ErrorStatus.java
@@ -21,12 +21,16 @@ public enum ErrorStatus {
     INVALID_CATEGORY(400, "ERROR - 유효하지 않은 카테고리 값이 포함되어 있습니다"),
 
     // <=============== 챌린지 ===============>
-    BAD_REQUEST_CHALLENGE  (400, "ERROR - 잘못된 챌린지 요청"),
-    BAD_REQUEST_APPLY      (400, "ERROR - 잘못된 신청입니다."),
-    INVALID_DATE_FORMAT    (400,"yyyy-MM-dd 형식으로 입력해주세요."),
-    NOT_FOUND_CHALLENGE    (404, "ERROR - 해당 챌린지를 찾을 수 없습니다."),
-    DUPLICATE_APPLY (409, "ERROR - 이미 신청하신 챌린지입니다."),
-    OVER_CAPACITY_CHALLENGE(422, "ERROR - 챌린지 신청 인원이 초과되었습니다."),
+    BAD_REQUEST_CHALLENGE(400, "ERROR - 잘못된 챌린지 요청"),
+    INVALID_DATE_FORMAT  (400,"yyyy-MM-dd 형식으로 입력해주세요."),
+    NOT_FOUND_CHALLENGE  (404, "ERROR - 해당 챌린지를 찾을 수 없습니다."),
+
+    // <=============== 챌린지 신청 ===============>
+    BAD_REQUEST_APPLICATION     (400, "ERROR - 잘못된 신청입니다."),
+    NOT_FOUND_APPLICATION       (404, "ERROR - 존재하지 않는 신청 항목입니다."),
+    DUPLICATE_APPLICATION       (409, "ERROR - 이미 신청하신 챌린지입니다."),
+    ALREADY_DECISION_APPLICATION(409, "ERROR - 해당 신청은 이미 승인 또는 거절 처리되었습니다."),
+    OVER_CAPACITY_APPLICATION   (422, "ERROR - 챌린지 신청 인원이 초과되었습니다."),
 
     // <=============== JWT ===============>
     INVALID_TOKEN  (401,"ERROR - 유효하지 않은 토큰입니다."),
